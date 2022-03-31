2de56a1e5b4dae53703e064aee0924bc5d546e7e
I483898 <j.vanefferen@student.fontys.nl>
2022-03-31 13:54:09 +0200

    back to where we were (prototype working)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,34 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                const iDiv = document.createElement('div');
+                var idstring = "widget";
+                console.log(idstring);
+                iDiv.id = idstring;
+                iDiv.style.height = "200px";
+                iDiv.style.width = "100%";
+                iDiv.style.backgroundColor = "black";
+                iDiv.style.margin = "10px";
+                document.getElementById('widgetlist').appendChild(iDiv);
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+                const iH1 = document.createElement('h1');
+                iH1.textContent = 'This is a widget!'
+                iH1.style.color = "white";
+                iDiv.appendChild(iH1);
+                //this.count++
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+    <div id="widgetlist"></div>
+    <button @click="increment">count is: {{ count }}</button>
+</template>

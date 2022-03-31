f44229aca6dd7f35e509839e1d0f08b914b80f07
rowandeschaux <483907@student.fontys.nl>
2022-03-31 11:05:17 +0200

    set up vue

---
+++ client/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+const path = require("path");
+
+module.exports = {
+ outputDir: path.resolve(__dirname, "../public")
+}

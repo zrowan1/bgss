39332a115b7b416912ed03814dc90ca8baa32317
rowandeschaux <483907@student.fontys.nl>
2022-03-31 10:51:54 +0200

    Initial commit backend

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const host = http//localhost
+const port = 3000
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`App is listening on ${host}:${port}`);
+});
+

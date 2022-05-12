067e803ed4b8a16488b3f5ba04ef892238951e61
I483898 <j.vanefferen@student.fontys.nl>
2022-05-12 16:01:22 +0200

    edit config updates with correct values

--- client/src/views/BrandGuide.vue
+++ client/src/views/BrandGuide.vue
@@ -7,7 +7,7 @@
     import { createApp, ref, onMounted } from 'vue'
     import router from '@/router'
     let editindex = -1
-    let naam = 4
+    let naam = 0
     let edit = false
     var isopen = ref()
     var waitForInput = ref()
@@ -28,12 +28,12 @@
     const widgets = ref([])
 
 
-    const starbucksBg = [{ "id": 4, "textContent": "Starbucks", "textColor": "white", "bgColor": "#006241", "textFont": "Koulen", "fontSize": "70px", "marginTop": "30px", "marginBottom": "30px", "widgetHeight": "auto", "type": 1 },
-        { "id": 6, "textContent": "As we evolve to meet beautifully diverse customers all over the world, our brand has evolved too. Here we introduce a fresh new design system that maintains the core elements of our brand while keeping our customers’ experience central to creative expression.  To achieve this, we’re thoughtfully incorporating beautiful, expressive moments with calm confidence in ways that are optimistic, joyful and recognizably Starbucks. By consistently utilizing the Siren logo, an expanded palette of greens rooted in our iconic green apron and a constrained family of harmonious typefaces, we bring purpose and cohesion to every interaction customers have with our brand.  From farmers, roasters and baristas to writers, designers and illustrators, we believe in the power of both coffee and art to connect people and communities. Our new creative expression marries the artful core of our brand with helping our customers where they are, on their terms.", "textColor": "black", "bgColor": "#d4e9e2", "textFont": "Koulen", "fontSize": "30px", "marginTop": "30px", "marginBottom": "30px", "widgetHeight": "auto", "type": 0 },
-        { "id": 9, "textContent": "Colors", "textColor": "black", "bgColor": "#f2f0eb", "textFont": "Koulen", "fontSize": "60px", "marginTop": "60px", "marginBottom": "20px", "widgetHeight": "auto", "type": 1 },
-        { "id": 9, "textContent": "Our green is iconic. Visible for blocks. It’s our most identifiable asset, from the color of our aprons to our logo.", "textColor": "black", "bgColor": "#f2f0eb", "textFont": "Koulen", "fontSize": "35px", "marginTop": "10px", "marginBottom": "20px", "widgetHeight": "auto", "type": 0 },
-        { "id": 9, "textContent": "We’re leaning into a family of greens to leverage brand recognition. Fresh and inviting, this expanded palette nods subtly to our heritage and propels us into a global future. We always ensure a presence of brand greens, either within the composition or through the presence of a logo.", "textColor": "black", "bgColor": "#f2f0eb", "textFont": "Koulen", "fontSize": "25px", "marginTop": "2px", "marginBottom": "20px", "widgetHeight": "auto", "type": 0 },
-        { "id": 9, "colors": ["#1e3932", "#006241", "#00754a", "#d4e9e2", "#ffffff", "#f9f9f9", "#f2f0eb", "#000000"], "bgColor": "#f2f0eb", "marginTop": "15px", "marginBottom": "20px", "widgetHeight": "400px", "type": 2 }]
+    const starbucksBg = [{ "id": 1, "textContent": "Starbucks", "textColor": "white", "bgColor": "#006241", "textFont": "Koulen", "fontSize": "70px", "marginTop": "30px", "marginBottom": "30px", "widgetHeight": "auto", "type": 1 },
+        { "id": 2, "textContent": "As we evolve to meet beautifully diverse customers all over the world, our brand has evolved too. Here we introduce a fresh new design system that maintains the core elements of our brand while keeping our customers’ experience central to creative expression.  To achieve this, we’re thoughtfully incorporating beautiful, expressive moments with calm confidence in ways that are optimistic, joyful and recognizably Starbucks. By consistently utilizing the Siren logo, an expanded palette of greens rooted in our iconic green apron and a constrained family of harmonious typefaces, we bring purpose and cohesion to every interaction customers have with our brand.  From farmers, roasters and baristas to writers, designers and illustrators, we believe in the power of both coffee and art to connect people and communities. Our new creative expression marries the artful core of our brand with helping our customers where they are, on their terms.", "textColor": "black", "bgColor": "#d4e9e2", "textFont": "Koulen", "fontSize": "30px", "marginTop": "30px", "marginBottom": "30px", "widgetHeight": "auto", "type": 0 },
+        { "id": 3, "textContent": "Colors", "textColor": "black", "bgColor": "#f2f0eb", "textFont": "Koulen", "fontSize": "60px", "marginTop": "60px", "marginBottom": "20px", "widgetHeight": "auto", "type": 1 },
+        { "id": 4, "textContent": "Our green is iconic. Visible for blocks. It’s our most identifiable asset, from the color of our aprons to our logo.", "textColor": "black", "bgColor": "#f2f0eb", "textFont": "Koulen", "fontSize": "35px", "marginTop": "10px", "marginBottom": "20px", "widgetHeight": "auto", "type": 0 },
+        { "id": 5, "textContent": "We’re leaning into a family of greens to leverage brand recognition. Fresh and inviting, this expanded palette nods subtly to our heritage and propels us into a global future. We always ensure a presence of brand greens, either within the composition or through the presence of a logo.", "textColor": "black", "bgColor": "#f2f0eb", "textFont": "Koulen", "fontSize": "25px", "marginTop": "2px", "marginBottom": "20px", "widgetHeight": "auto", "type": 0 },
+        { "id": 6, "colors": ["#1e3932", "#006241", "#00754a", "#d4e9e2", "#ffffff", "#f9f9f9", "#f2f0eb", "#000000"], "bgColor": "#f2f0eb", "marginTop": "15px", "marginBottom": "20px", "widgetHeight": "400px", "type": 2 }]
 
 
 
@@ -48,10 +48,13 @@
 
     function loadStarbucksBg() {
         widgets.value = [...starbucksBg]
+        naam = 7
     }
 
-    function removeWidget(plek) {
-        widgets.value.splice(plek, 1)
+    function removeWidget(id) {
+        let i = widgets.value.map(item => item.id).indexOf(id)
+        console.log(i)
+        widgets.value.splice(i, 1)
     }
 
     function openConfig(propArray) {
@@ -92,19 +95,45 @@
         };
         if (edit === false) {
             widgets.value.push(newWidget)
-            console.log(widgets.value.length);
             naam++;
         }
         else {
-            widgets.value.splice(editindex, 1)
-            widgets.value.splice(editindex, 0, newWidget)
+
+            let i = widgets.value.map(item => item.id).indexOf(editindex)
+            widgets.value.splice(i, 1)
+            widgets.value.splice(i, 0, newWidget)
+            naam++
             edit = false
         }
     }
 
+    function fillConfig(id) {
+        
+        var oldWidget = getWidgetFromArray(id)
+        CtextContent.value = oldWidget.textContent
+        CtextColor.value = oldWidget.textColor
+        CbgColor.value = oldWidget.bgColor
+        CtextFont.value = oldWidget.textFont
+        CtextSize.value = oldWidget.fontSize
+        CmarginTop.value = oldWidget.marginTop
+        CmarginBottom.value = oldWidget.marginBottom
+        CborderType.value = oldWidget.borderType
+        CborderSize.value = oldWidget.borderSize
+        CborderColor.value = oldWidget.borderColor
+        CwidgetHeight.value = oldWidget.widgetHeight
+        CpaletteColors.value = [...oldWidget.colors]
+        
+    }
+
     function addWidgetPressed(index) {
         editindex = index
+        if (editindex >= 0) {
+            edit = true
+        }
         const propList = ["textContent", "textColor", "bgColor", "textFont", "fontSize", "marginTop", "marginBottom", "widgetHeight"];
+        if (edit) {
+            fillConfig(index)
+        }
         openConfig(propList);
         waitForInput.value = true
         checkConfigInput(addWidgetCallback)
@@ -112,7 +141,13 @@
 
     function addWidgetHeaderPressed(index) {
         editindex = index
+        if (editindex >= 0) {
+            edit = true
+        }
         const propList = ["textContent", "textColor", "bgColor", "textFont", "fontSize", "marginTop", "marginBottom", "widgetHeight"];
+        if (edit) {
+            fillConfig(index)
+        }
         openConfig(propList);
         waitForInput.value = true
         checkConfigInput(addWidgetHeaderCallback)
@@ -121,9 +156,7 @@
     
 
     function addWidgetHeaderCallback() {
-        if (editindex >= 0) {
-            edit = true
-        }
+        
         const newWidget = {
             id: naam,
             textContent: CtextContent.value,
@@ -136,21 +169,30 @@
             widgetHeight: CwidgetHeight.value,
             type: 1,
         };
-        if (edit === false) {
+        if (!edit) {
             widgets.value.push(newWidget)
             console.log(widgets.value.length);
             naam++;
         }
         else {
-            widgets.value.splice(editindex, 1)
-            widgets.value.splice(editindex, 0, newWidget)
+
+            let i = widgets.value.map(item => item.id).indexOf(editindex)
+            widgets.value.splice(i, 1)
+            widgets.value.splice(i, 0, newWidget)
+            naam++
             edit = false
         }
     }
 
     function addWidgetColorPalettePressed(index) {
         editindex = index
+        if (editindex >= 0) {
+            edit = true
+        }
         const propList = ["paletteColors", "bgColor", "marginTop", "marginBottom", "widgetHeight"];
+        if (edit) {
+            fillConfig(index)
+        }
         openConfig(propList);
         waitForInput.value = true
         checkConfigInput(addWidgetColorPaletteCallback)
@@ -178,12 +220,25 @@
             naam++;
         }
         else {
-            widgets.value.splice(editindex, 1)
-            widgets.value.splice(editindex, 0, newWidget)
+
+            let i = widgets.value.map(item => item.id).indexOf(editindex)
+            widgets.value.splice(i, 1)
+            widgets.value.splice(i, 0, newWidget)
+            naam++
             edit = false
         }
     }
 
+
+    function getWidgetFromArray(id) {
+        for (var i = 0; i < widgets.value.length; i++) {
+            if (widgets.value[i].id === id) {
+                return widgets.value[i]
+            }
+        }
+    }
+
+  
 
 
 
@@ -194,10 +249,11 @@
 
     <div class="body">
         <div>
+            <button @click="test()"> test</button>
             <button @click="goToCms()"> Ga Terug</button>
             <button @click="loadStarbucksBg()">Starbucks</button>
         </div>
-        <template v-for="(widget,index) in widgets"
+        <template v-for="widget in widgets"
                   :key="widget.id">
             <widget v-if="widget.type === 0"
                     :textContent="widget.textContent"
@@ -208,9 +264,9 @@
                     :marginTop="widget.marginTop"
                     :marginBottom="widget.marginBottom"
                     :widgetHeight="widget.widgetHeight"
-                    :id="index"
+                    :id="widget.id"
                     v-on:remove-click="(n) => removeWidget(n)"
-	                v-on:editwidget="(n) => addWidgetPressed(index)"
+	                v-on:editwidget="(n) => addWidgetPressed(widget.id)"
                     >
             </widget>
             <widgetHeader v-else-if="widget.type === 1"
@@ -222,9 +278,9 @@
                           :marginTop="widget.marginTop"
                           :marginBottom="widget.marginBottom"
                           :widgetHeight="widget.widgetHeight"
-                          :id="index"
+                          :id="widget.id"
                           v-on:remove-click="(n) => removeWidget(n)"
-	                      v-on:editwidgetHeader="(n) => addWidgetHeaderPressed(index)">
+	                      v-on:editwidgetHeader="(n) => addWidgetHeaderPressed(widget.id)">
 
                           
 
@@ -235,9 +291,9 @@
                                 :marginTop="widget.marginTop"
                                 :marginBottom="widget.marginBottom"
                                 :widgetHeight="widget.widgetHeight"
-                                :id="index"
+                                :id="widget.id"
                                  v-on:remove-click="(n) => removeWidget(n)"
-	                             v-on:editwidgetcolorPalette="(n) => addWidgetColorPalettePressed(index)">
+	                             v-on:editwidgetcolorPalette="(n) => addWidgetColorPalettePressed(widget.id)">
                                 >
 
             </widgetColorPalette>
